ada0f6f53980f29c6a1cd02e06ce4a3ffd4cccf4
Tobias Lüger <tobias-lueger@web.de>
2024-05-20 20:22:58 +0200

    fixed typescript button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { cva } from "class-variance-authority";
 
 const button = cva("font-semibold px-[18px] py-[8px] rounded-[5px] border-secondary border-[1px] transition hover:shadow-[0_0_10px_1px_rgb(234,250,97)]", {
@@ -14,13 +14,20 @@ const button = cva("font-semibold px-[18px] py-[8px] rounded-[5px] border-second
   }
 });
 
+interface ButtonProps {
+  children: string | React.ReactNode,
+  type?: "primary" | "secondary",
+  href?: string,
+  className?: string
+}
+
 const Button = ({
   children,
   className,
   type,
   href,
   ...props
-}) => {
+}: ButtonProps) => {
   return (
     <a href={href || "#"} className={`${button({ type })} ${className}`} {...props}>{children}</a>
   )
--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -1,7 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react';
-import Image from "next/image";
-import Link from 'next/link';
+import React from 'react';
 import Button from '../Button/Button';
 
 const Stage = () => {
